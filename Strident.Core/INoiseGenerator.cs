b4e319fffd71303e80679e000d38c27641a72410
Tom Keschl <thomas.keschl@gmail.com>
2015-01-18 08:07:40 -0500

    Initial commit.

---
+++ Strident.Core/INoiseGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strident.Core
+{
+    interface INoiseGenerator
+    {
+        double noiseAt(double x, double y);
+
+        double noiseAt(double x, double y, double z);
+    }
+}
---
+++ Strident.Core/SimplexNoiseGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strident.Core
+{
+    public class SimplexNoiseGenerator : INoiseGenerator
+    {
+        public double noiseAt(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double noiseAt(double x, double y, double z)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
