d5c18586f7c45481f3c5651e7a79c65a5c622a56
Tom Keschl <thomas.keschl@gmail.com>
2015-01-18 09:58:23 -0500

    Initial commit.

---
+++ Strident.Core/NoiseGeneratorFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strident.Core
+{
+    class NoiseGeneratorFactory
+    {
+        public INoiseGenerator createSimplexGenerator() {
+            return new SimplexNoiseGenerator();
+        }
+    }
+}
